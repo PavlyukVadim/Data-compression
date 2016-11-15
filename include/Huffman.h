72c5fb90272dfec839776b101f245f397c60efdb
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2016-11-16 01:10:09 +0200

    create basic classes

--- include/Huffman.h
+++ include/Huffman.h
@@ -1,4 +1,5 @@
 
+
 class Huffman
 {
     public:
@@ -6,4 +7,37 @@ class Huffman
         virtual ~Huffman();
 
 };
+
+class BasicNode {
+    public:
+        int freq;
+        BasicNode();
+        BasicNode(int f) { freq = f;}
+        virtual ~BasicNode() {};
+};
+
+
+class ParentNode : public BasicNode {
+    public:
+       BasicNode* left;
+       BasicNode* right;
+
+       ParentNode(BasicNode* l, BasicNode* r) : BasicNode(l->freq + r->freq), left(l), right(r) {}
+       ~ParentNode() {
+            delete left;
+            delete right;
+       }
+};
+
+
+class LeafNode : public BasicNode {
+    public:
+        const char c;
+        LeafNode(int f, char c) : BasicNode(f), c(c) {}
+};
+
+
+
+
+
 
