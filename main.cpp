18eaef30e63809166d85af3e6f1dd95d2fd40feb
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2016-11-22 01:37:14 +0200

    initial Huffman for binary files

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
 #include "Archiver.h"
-#include "Huffman.h"
+#include "BHuffman.h"
+
+
 
 using namespace std;
 
 
 int main()
 {
-    vector<string> files(3);
+    //vector<string> files(3);
 
     /*files[0] = "aaa.txt";
     files[1] = "bbb.txt";
@@ -21,10 +23,12 @@ int main()
     ar->Compression();*/
     //ar->Decompression("arc");
     //setlocale(LC_ALL, "Russian");
-    Huffman* hf = new Huffman();
+    //Huffman* hf = new Huffman();
     //string TestString = "idaoisufh lasjdvhkjs sidfbdfh dajsb zlkxvj lksjzfzlkjcxlk";
     //hf->Compression("RCT.txt");
-    hf->Decompression("huffmanCom.bin", "decompress.txt");
+    //hf->Decompression("huffmanCom.bin", "decompress.txt");
+
+
 
     return 0;
 }
